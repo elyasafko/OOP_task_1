813ab964c7f7cfcf2126f7f71144bca6de1882cd
elyasaf <elyasafko@gmail.com>
2024-01-15 13:08:25 +0200

    added kill function (only left)
    Signed-off-by: elyasaf <elyasafko@gmail.com>

--- GameLogic.java
+++ GameLogic.java
@@ -83,8 +83,42 @@ public class GameLogic implements PlayableLogic
         // move the piece
         board[b.getColumn()][b.getRow()] = board[a.getColumn()][a.getRow()];
         board[a.getColumn()][a.getRow()] = null;
+        // kill check
+        if (board[b.getColumn()][b.getRow()] instanceof Pawn)
+            kill(b);
         // return true if the move was successful
         return true;
+    }
+
+    public void kill(Position b)
+    {
+        int X = b.getColumn();
+        int Y = b.getRow();
+        //check left
+        if ((X != 0) && (board[X - 1][Y]!= null) &&(board[X - 1][Y].getOwner() != board[X][Y].getOwner()))
+        {
+            if (board[X - 1][Y] instanceof Pawn)
+            {
+                // check if we close to a wall
+                if (X == 1)
+                {
+                    board[X - 1][Y] = null;
+                    ((Pawn) board[X][Y]).AddKillCount();
+                }
+                // check if we close to a corner
+                else if (X == 2 && (Y == 0) || (X == 10))
+                {
+                    board[X - 1][Y] = null;
+                    ((Pawn) board[X][Y]).AddKillCount();
+                }
+                else if (((board[X - 2][Y]!= null)) && (board[X - 2][Y].getOwner() == board[X][Y].getOwner()) && (board[X - 2][Y] instanceof Pawn))
+                {
+                    board[X - 1][Y] = null;
+                    ((Pawn) board[X][Y]).AddKillCount();
+                    ((Pawn) board[X-2][Y]).AddKillCount();
+                }
+            }
+        }
     }
 
     private int calculateSteps(Position a, Position b)
--- Position.java
+++ Position.java
@@ -4,7 +4,7 @@ public class Position
     private int column;
     private boolean[] BeenThereDoneThat;
 
-    public Position(int row, int column)
+    public Position(int column, int row)
     {
         this.row = row;
         this.column = column;
